c3b04f4796ed120ba279425fa054522bfc430125
Ryan Wise <ryan.wise@wiseweb-design.com>
2015-05-29 17:58:16 -0400

    irst commit

---
+++ Assets/scripts/CameraFollowOnX.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class CameraFollowOnX : MonoBehaviour {
+
+	public GameObject player;
+
+	void Update () 
+	{
+		Vector3 newTransform = transform.position;
+		newTransform.x = player.transform.position.x;
+		transform.position = newTransform;
+	}
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float jumpSpeed = 1f;
+	public float velocity = 1f;
+	private bool canJump = true;
+	Rigidbody2D rb;
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update () 
+	{
+		if (Input.GetMouseButton(0) && canJump)
+			jump();
+		else
+			rb.velocity = new Vector2(velocity, rb.velocity.y);
+		if (!Input.GetMouseButton(0))
+		{
+			canJump = true;
+		}
+	}
+	void jump()
+	{
+		rb.velocity = new Vector2(velocity, jumpSpeed);
+		canJump = false;
+	}
+	void OnCollisionEnter(Collision other)
+	{
+		//do a gameover
+	}
+}
