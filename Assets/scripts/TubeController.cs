95682919bb5667ffd9b149301d33b89e7270688e
Ryan Wise <ryan.wise@wiseweb-design.com>
2015-05-30 00:57:59 -0400

    Random Tube generation

--- Assets/scripts/TubeController.cs
+++ Assets/scripts/TubeController.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class TubeController : MonoBehaviour {
+public class TubeController : MonoBehaviour
+{
+	//y = -1.6
+	public GameObject[] tubes;
+	public GameObject player;
+	List<GameObject> liveTubes;
 
-	// Use this for initialization
-	void Start () {
-	
+	void Start()
+	{
+		liveTubes = new List<GameObject>();
+		InvokeRepeating("spawnTube", 0f, 4f);
+		InvokeRepeating("DestroyTube", 8f,4f);
+		Random.seed = 42;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void spawnTube()
+	{
+		int tubeGroup = Random.Range(0, 4);
+		Vector2 newPos = player.transform.position;
+		newPos.y = -1.6f;
+		newPos.x += 50f;
+		liveTubes.Add ( Instantiate(tubes[tubeGroup], newPos, Quaternion.identity) as GameObject);
 	}
+	void DestroyTube()
+	{
+		Destroy(liveTubes[0]);
+		liveTubes.RemoveAt(0);
+	}
+
 }
