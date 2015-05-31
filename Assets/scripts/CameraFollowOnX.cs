54746db6883320c0d8f4e5515369def036e172bc
Ryan Wise <ryan.wise@wiseweb-design.com>
2015-05-30 22:24:15 -0400

    Gameplay improvements

--- Assets/scripts/CameraFollowOnX.cs
+++ Assets/scripts/CameraFollowOnX.cs
@@ -5,11 +5,11 @@ using System.Collections;
 public class CameraFollowOnX : MonoBehaviour {
 
 	public GameObject player;
-
+	public float offset = 0f;
 	void Update () 
 	{
 		Vector3 newTransform = transform.position;
-		newTransform.x = player.transform.position.x;
+		newTransform.x = player.transform.position.x + offset;
 		transform.position = newTransform;
 	}
 }
--- Assets/scripts/TubeController.cs
+++ Assets/scripts/TubeController.cs
@@ -7,21 +7,40 @@ public class TubeController : MonoBehaviour
 	//y = -1.6
 	public GameObject[] tubes;
 	public GameObject player;
+	public float offset;
 	List<GameObject> liveTubes;
 
 	void Start()
 	{
 		liveTubes = new List<GameObject>();
-		InvokeRepeating("spawnTube", 0f, 4f);
-		InvokeRepeating("DestroyTube", 8f,4f);
+		InvokeRepeating("spawnTube", 0f, 1f);
+		InvokeRepeating("DestroyTube", 2f,2f);
 		Random.seed = 42;
 	}
 	void spawnTube()
 	{
-		int tubeGroup = Random.Range(0, 4);
+		int tubeGroup = Random.Range(1, 5);
 		Vector2 newPos = player.transform.position;
-		newPos.y = -1.6f;
-		newPos.x += 50f;
+		switch (tubeGroup + 1) 
+		{
+		case 1:
+			offset = -15;
+			break;
+		case 2:
+			offset = 1f;
+			break;
+		case 3:
+			offset = -9;
+			break;
+		case 4:
+			offset = -4;
+			break;
+		case 5:
+			offset = 0;
+			break;
+		}
+		newPos.y = offset;
+		newPos.x += 30f;
 		liveTubes.Add ( Instantiate(tubes[tubeGroup], newPos, Quaternion.identity) as GameObject);
 	}
 	void DestroyTube()
